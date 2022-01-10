7770a1b73709e8fa7b843fe94d2e2f1ed58925bc
Bogdan Boiculese <byte1918@gmail.com>
2022-01-10 23:41:38 +0000

    count how many tasks left, better messages

--- src/pages/choresPage/choreListItem.tsx
+++ src/pages/choresPage/choreListItem.tsx
@@ -2,7 +2,7 @@ import IconButton from '@mui/material/IconButton';
 import CheckIcon from '@mui/icons-material/Check';
 import ListItemText from '@mui/material/ListItemText';
 import ListItem from '@mui/material/ListItem';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { toast } from 'react-toastify';
 import { completeChore } from 'srcRootDir/services/chores';
 import styled from '@emotion/styled';
@@ -11,10 +11,20 @@ export function ChoreListItem({ chore, onComplete }: { chore: Chore; onComplete?
   const [showSecondaryText, setShowSecondaryText] = useState(false);
 
   const handleCompleteClick = useCallback(async (chore: Chore) => {
-    toast.success(`Completed '${chore.title}'`);
+    toast.success(`Completed « ${chore.title} »`);
     await completeChore(chore.id);
     onComplete && onComplete();
   }, []);
+
+  const title = useMemo(() => {
+    const description = chore.description ? '…' : '';
+    const count = convertCompletionSemaphoreToCount(chore.completionSemaphore);
+    if (count > 1) {
+      return `${chore.title}${description} (${count})`;
+    } else {
+      return `${chore.title}${description}`;
+    }
+  }, [chore]);
 
   return (
     <ListItem
@@ -26,11 +36,17 @@ export function ChoreListItem({ chore, onComplete }: { chore: Chore; onComplete?
       }
     >
       <ListItemText
-        primary={`${chore.title}${chore.description ? '…' : ''}`}
+        primary={title}
         secondary={showSecondaryText && chore.description ? <Description>{chore.description}</Description> : undefined}
       />
     </ListItem>
   );
+}
+
+function convertCompletionSemaphoreToCount(completionSemaphore: number) {
+  const alignment = completionSemaphore >= 0 ? 1 : 0;
+
+  return Math.abs(completionSemaphore) + alignment;
 }
 
 const Description = styled.span`
