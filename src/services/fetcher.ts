d31870741e356b1f3002a581b2a630f59a8aaa96
Bogdan Boiculese <byte1918@gmail.com>
2022-01-10 21:40:32 +0000

    configure BACKEND_ORIGIN

--- src/services/fetcher.ts
+++ src/services/fetcher.ts
@@ -1,12 +1,12 @@
 import { getCurrentUserId } from 'srcRootDir/services/auth';
 
-// const BACKEND_ORIGIN = 'http://localhost:4000';
-const BACKEND_ORIGIN = 'https://chores-api.onrender.com';
-
 export function fetcher(url: string, init?: RequestInit): any | Response {
   const userId = getCurrentUserId();
 
-  return fetch(`${BACKEND_ORIGIN}${url}`, { headers: { 'X-UserId': userId.toString() }, ...(init || {}) }).then(res => {
+  return fetch(`${BACKEND_ORIGIN}${url}`, {
+    headers: { 'X-UserId': userId.toString() },
+    ...(init || {}),
+  }).then(res => {
     if (res.status === 200) {
       return res.json();
     }
